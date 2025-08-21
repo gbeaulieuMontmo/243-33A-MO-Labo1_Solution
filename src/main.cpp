b12d7db8436cfbfef7c4bb6b651c373702aa78eb
gbeaulieuMontmo <Guillaume.Beaulieu@cmontmorency.qc.ca>
2025-08-21 09:43:01 -0400

    Ajout manip 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
+/**************************************************************/
+// Date de création du programme: 2 avril 2025
+// Date de la dernière modification: 4 avril 2025
+// Nom du programmeur principal: Guillaume Beaulieu
+//
+// But du programme: solution des exercices du laboratoire 1 du 
+// cours de programmation 243-33A-MO Microcontrôleur 2. Chaque
+// exercices est implémenté dans une fonction distincte. Il suffit
+// d'appeler la fonction dans le loop() pour exécuter l'exercice.
+//
+// Version: 1.0
+/**************************************************************/
+
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void manipulation1() {
+  Serial.println("Bonjour le monde!");  
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  manipulation1();
+  delay(1000);
 }
